ed5cbfd7958be559f2a37b314263fb1b8f57f639
jazzthedog <jazzthedog.bowwow@gmail.com>
2015-11-08 23:15:08 -0500

    modified the has_attached_file spec

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,7 +7,7 @@ class Book < ActiveRecord::Base
   belongs_to :user
   has_many :sales  
   has_many :comments
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100>" }
   has_attached_file :resource
 
   validates_attachment_content_type :image, 
